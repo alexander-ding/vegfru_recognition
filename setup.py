42d16af4241af4d957d65a3a3b143407155fbd67
Alexander Ding <alexding123@outlook.com>
2019-03-26 19:15:46 -0400

    Initial setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An energy-efficient model for classifying vegetables and fruits',
+    author='Alex Ding',
+    license='MIT',
+)
